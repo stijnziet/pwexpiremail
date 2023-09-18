b5a20b5ba2f85eb9637efa3a62759d062852f376
Stijn <86265331+stijnziet@users.noreply.github.com>
2023-09-18 11:02:18 +0200

    Add files via upload

---
+++ autoupdate.ps1
@@ -0,0 +1,7 @@
+﻿Set-ExecutionPolicy bypass -force
+$ConfirmPreference = 'None'
+Install-Module PSWindowsUpdate -Force
+Import-Module PSWindowsUpdate -Force
+Get-WindowsUpdate -AcceptAll -Install
+Set-ExecutionPolicy restricted -force
+write-host "please reboot"
