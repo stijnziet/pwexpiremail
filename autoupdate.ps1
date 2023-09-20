f1597938b19a6f8bca0386ad5bca93dfc5c3b8db
Stijn <86265331+stijnziet@users.noreply.github.com>
2023-09-20 16:14:06 +0200

    Update autoupdate.ps1

--- autoupdate.ps1
+++ autoupdate.ps1
@@ -1,7 +1,8 @@
 Set-ExecutionPolicy bypass -force
 $ConfirmPreference = 'None'
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name NuGet -Force
 Install-Module PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate -Force
 Get-WindowsUpdate -AcceptAll -Install
 Set-ExecutionPolicy restricted -force
-write-host "please reboot"
